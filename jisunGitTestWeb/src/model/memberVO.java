d9beaca4b6f8db6c8e12ea8038fa5b574d6bcdb4
JISUN <JISUN@192.168.0.104>
2018-08-28 14:40:30 +0900

    id 추가했습니당

--- jisunGitTestWeb/src/model/memberVO.java
+++ jisunGitTestWeb/src/model/memberVO.java
@@ -1,5 +1,19 @@
 package model;
 
 public class memberVO {
+	private String id;
+	
+	public memberVO(String id) {
+		super();
+		this.id = id;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 }
